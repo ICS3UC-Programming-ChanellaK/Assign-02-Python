4a13699ef985c876b74bfb8c319aae19e414fe9e
Chanella Keza <chanella.keza@stu.ocsb.ca>
2025-03-17 15:43:12 +0000

    added calc.py

---
+++ Assign/calc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Created By: chanella
+# Date: feb 27, 2025
+# This program calculates the volume and the surface area of a triangular prism
+# with dimensions 4.982 * 2.122 * 8.431 (B.H.L)
+# with HYP 9.882
+
+
+def main():
+    # This function calculates the surface area of a triangular prism
+    print("If the surface area dimensions")
+    print("4.982 * 2.122 * 8.431")
+    print("")
+    print("Area is{}cm^2".format(0.5 * 4.982 * 2.122 * 8.431))
+
+    # This function calculates the perimeter of a triangular prism
+    print("If the surface area dimensions")
+    print("4.982cm x 8.431cm")
+    print("")
+    print("perimeter is{}cm".format(4.982 + 2.122 + 8.431))
+
+
+if __name__ == "__main__":
+    main()
